c1baabd5e651f559179a59fd785ef542d40bce2a
Hassan Berrada <hassanberrada@Hassans-Air-2.lan>
2024-04-14 21:14:10 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,23 @@
 // Steps for the BackTesting Engine. 
 // 1 - Read the data 
 // 2 - Implement ONE Trend Following Channel With DrawdownControl
-// 2 - Create a loop with 
-// 3 - I
+// 2a - Handle P&L with Slippage (See TF Data for Slippage in col. V)
+// 2b - Create useful metrics (Measure various statistical performance properties of both equity curve and trade-by-trade table: Average Rate of Return, Standard Deviation of Returns, and the Ratio of the two above (known as the Sharpe or Information Ratio), Total Number of Trades, % of Winners or % of Profitable Trades, Return on the Account (Net Profit to Worst Drawdown), Average Winner, Average Loser, Profit Factor, etc. along with all other characteristics that you will deem useful to gaining insight into the behavior of your market. The quantity and choices of the performance characteristics will make a difference on the grade for your Final Project.)
+// 3 - Create a loop to optimize 
+
+
+// 3 - Pseudo code for 3. 
+void pseudoCodeParmaterSearch() {
+    for (int ChnLen = 500; ChnLen < 10000; ChnLen++) {
+        for (double StpPct = 0.005; StpPct <= 0.10; StpPct += 0.001  ) {
+            std::cout << "Run Strategy" << std::endl;
+        }
+    }
+}
+
+// 2a - Need to handle this constant
+const double SLPG = 1;
+
 struct Bar {
     std::string date;
     std::string time;
@@ -49,13 +64,15 @@ std::vector<Bar> readData(const std::string& filename) {
 }
 
 int main() {
-    std::string filename = "../HO-5minHLV.csv"; // Replace with your actual file name
+    std::string filename = "../data/HO-5minHLV.csv"; // Replace with your actual file name
     auto bars = readData(filename);
 
     // Example: print the close prices
     for (const auto& bar : bars) {
         std::cout << "Close: " << bar.close << std::endl;
     }
+
+    pseudoCodeParmaterSearch();
 
     return 0;
 }
